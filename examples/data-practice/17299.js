d6e1dfc8d99f5dfc91e59cbde1dcc534d44b8b4c
고우영 <wooyoung.ko@nts-corp.com>
2022-11-16 17:17:52 +0900

    [자료구조 연습] 오큰수 문제풀이 수정

--- examples/data-practice/17299.js
+++ examples/data-practice/17299.js
@@ -1,18 +1,20 @@
-// https://www.acmicpc.net/problem/17298
+// https://www.acmicpc.net/problem/17299
 
-const input = require('fs').readFileSync('17298.txt').toString().trim().split('\n');
+const input = require('fs').readFileSync('17299.txt').toString().trim().split('\n');
 // const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const logs = [];
 const stack = [];
+const freq = {};
 const arr = input[1].split(' ')
 
 for (let j=0; j<arr.length; j++) {
   arr[j] = Number(arr[j]);
+  freq[arr[j]] = freq[arr[j]] ? freq[arr[j]] + 1 : 1;
 }
 
 stack.push(0);
 for (let i=1; i<arr.length; i++) {
-  while (stack.length > 0 && arr[stack[stack.length-1]] < arr[i]) {
+  while (stack.length > 0 && freq[arr[stack[stack.length-1]]] < freq[arr[i]]) {
     logs[stack.pop()] = arr[i];
   }
   stack.push(i)
